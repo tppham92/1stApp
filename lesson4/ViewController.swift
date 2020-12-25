ab209eea6a30c928155034e9a28b252c8dcbff35
tppham92 <partender92@gmail.com>
2020-12-25 19:07:32 +0700

    Understand IBAction and IBOutlets

--- lesson4/ViewController.swift
+++ lesson4/ViewController.swift
@@ -8,14 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var nounTextfield: UITextField!
+    @IBOutlet weak var adjTextfield: UITextField!
+    @IBOutlet weak var verbTextfield: UITextField!
+    @IBOutlet weak var numberSlider: UISlider!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    let storyPrompt = StoryPromptEntry()
+    
+    @IBAction func changNumber(_ sender: UISlider) {
+        numberLabel.text = "Number: \(Int(sender.value))"
+        storyPrompt.number = Int(sender.value)
+    }
+    @IBAction func changeStoryType(_ sender: UISegmentedControl) {
+        if let genre = StoryPrompts.Genre(rawValue: sender.selectSegmentIndex) {
+        storyPrompt.genre = genre
+    } else {
+    storyPrompt.genre = .scifi
+    }
+}
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let storyPrompt = StoryPromptEntry()
+        
+        numberSlider.value = 7.5
         storyPrompt.noun = "Toaster"
         storyPrompt.adjective = "Smelly"
         storyPrompt.verb = "Burps"
-        storyPrompt.number = 10
+        storyPrompt.number = Int(numberSlider.value)
         print(storyPrompt)
     }
 }
